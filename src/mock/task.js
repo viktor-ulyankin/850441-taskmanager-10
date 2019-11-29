680efa3d3733d9f30770fbc8d66d6dc28d042b1f
Viktor Ulyankin <freeman.viktor@mail.ru>
2019-11-29 23:45:33 +0300

    Модуль 3. Задание 1.

---
+++ src/mock/task.js
@@ -0,0 +1,73 @@
+import {Colors} from '../const.js';
+
+const DescriptionItems = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`,
+];
+
+const DefaultRepeatingDays = {
+  'mo': false,
+  'tu': false,
+  'we': false,
+  'th': false,
+  'fr': false,
+  'sa': false,
+  'su': false,
+};
+
+const Tags = [
+  `homework`,
+  `theory`,
+  `practice`,
+  `intensive`,
+  `keks`
+];
+
+const getRandomIntegerNumber = (min, max) => min + Math.floor((max + 1) * Math.random());
+
+const getRandomArrayItem = (array) => array[getRandomIntegerNumber(0, array.length)];
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 6);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
+};
+
+const generateRepeatingDays = () => {
+  return Object.assign({}, DefaultRepeatingDays, {
+    'mo': Math.random() > 0.5,
+  });
+};
+
+const generateTags = (tags) => {
+  return tags
+  .filter(() => Math.random() > 0.5)
+  .slice(0, 3);
+};
+
+const generateTask = () => {
+  const dueDate = Math.random() > 0.5 ? null : getRandomDate();
+
+  return {
+    description: getRandomArrayItem(DescriptionItems),
+    dueDate,
+    repeatingDays: dueDate ? DefaultRepeatingDays : generateRepeatingDays(),
+    tags: new Set(generateTags(Tags)),
+    color: getRandomArrayItem(Colors),
+    isFavorite: Math.random() > 0.5,
+    isArchive: Math.random() > 0.5,
+  };
+};
+
+const generateTasks = (count) => {
+  return new Array(count)
+  .fill(``)
+  .map(generateTask);
+};
+
+export {generateTask, generateTasks};
